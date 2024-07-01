369607fa8f57d5dffa6b3f9e01ba2a7261ace765
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2024-07-01 06:31:04 -0700

    Class work
    Class work

---
+++ 21-03-2024 class work/app.js
@@ -0,0 +1,58 @@
+"use strict";
+// let email:string = 'ali@gmail.com';
+// let password:string= "123456"
+// if(email == 'ali@gmail.com' && password === "123456"){
+//     console.log('you successfully login');
+// }
+// else{
+//     console.log('invalid username & password');
+// }
+// if(password.length < 6){
+//     console.log("please entered minimum six digit");
+// }
+// and visit slide no 148 to see (if else) condition
+// when typescript auto define type of data is called inference
+// let percentage = 39
+// if(percentage>=90){
+// console.log( 'A+ Grade');
+// }
+// else if (percentage >= 80 && percentage <=89){
+//     console.log('A Grade');
+// }
+// else if (percentage >= 70 && percentage <=79.99){
+//     console.log('B Grade');
+// }
+// else if (percentage >= 60 && percentage <=69.99){
+//     console.log('C Grade');
+// }
+// else if (percentage >= 50 && percentage <=59.99){
+//     console.log('D Grade');
+// }
+// else if (percentage >= 33 && percentage <=49.99){
+//     console.log('E Grade');
+// }
+// else{
+//     console.log('You are Fail');
+// }
+// type Shape = {
+//     kind: "circle" | "rectangle";
+//     radius?: number; // Only for circles
+//     width?: number; // Only for rectangles
+//     height?: number; // Only for rectangles
+// };
+// // Describing a circle using our Shape type
+// let circle: Shape = {
+//     kind: "circle",
+//     radius: 5
+// };
+// // Describing a rectangle using our Shape type
+// let rectangle: Shape = {
+//     kind: "rectangle",
+//     width: 10,
+//     height: 20
+// };
+// // Showing what we described
+// console.log(circle); // Here's the circle
+// console.log(rectangle); // And here's the rectangle
+// // We made a flexible program that can describe different shapes in deta
+// console.log(circle);
